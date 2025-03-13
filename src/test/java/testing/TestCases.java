88c2bf89d8b97a8d4eb5bd7efe731cf2a012da79
Balajibalakrishnan12 <balaji.bba2000@gmail.com>
2025-03-14 00:59:59 +0530

    Updated testng file

--- src/test/java/testing/TestCases.java
+++ src/test/java/testing/TestCases.java
@@ -42,62 +42,62 @@ public class TestCases {
 	// click&select dropdown
 	// @Test(dependsOnMethods="loginpage",alwaysRun = true )//always run - used to
 	// run the code if code fails also
-	@Test(priority = 2)
-	public void productpage() {
-		System.out.println("user is in product page");
-		ProductPage product = new ProductPage();
-		product.dropDownByIndex(2);
-//		 product.menuBar();
-//		 product.clickLogoutButton();
-		productnames.add("Sauce Labs Backpack");
-		productnames.add("Sauce Labs Bike Light");
-		product.productSelection(productnames);
-		// product.menuBar();
-		// product.clickLogoutButton();
-
-}
-
-	@BeforeMethod(groups="logingroup")
-	public void setup() {
-		DriverManager.getDriver("chrome");
-		DriverManager.url("https://www.saucedemo.com/v1/");
-		System.out.println("browser launched");
-		
-	}
-
-	@AfterSuite
-	public void teardown() {//
-	DriverManager.close();
-		System.out.println("browser close");
-	}
-	
-		@Test(priority=3)
-		public void productaddtocart() throws InterruptedException {
-			Addtocart cart = new Addtocart();
-			//cart.itemcartButton();
-			//cart.addtocartButton();
-			cart.container();
-			cart.checkingItemisAvailable(productnames);
-			List<String> item = new ArrayList<String>();
-			item.add("Sauce Labs Backpack");
-			item.add("Sauce Labs Bike Light");
-			
-			cart.checkout();
-			
-		}
-		@Test(priority=4)
-		public void processingpayment() {
-			Payment amount = new Payment();
-			amount.paymentofproducts("Balaji", "Balakrishnan", 605004);
-			
-		}
-		
-		@Test(priority = 5)
-		public void overallcalculation() {
-			FinalStatus overall=new FinalStatus();
-			overall.calculation();
-			overall.finish();
-		}
+//	@Test(priority = 2)
+//	public void productpage() {
+//		System.out.println("user is in product page");
+//		ProductPage product = new ProductPage();
+//		product.dropDownByIndex(2);
+////		 product.menuBar();
+////		 product.clickLogoutButton();
+//		productnames.add("Sauce Labs Backpack");
+//		productnames.add("Sauce Labs Bike Light");
+//		product.productSelection(productnames);
+//		// product.menuBar();
+//		// product.clickLogoutButton();
+//
+//}
+//
+//	@BeforeMethod(groups="logingroup")
+//	public void setup() {
+//		DriverManager.getDriver("chrome");
+//		DriverManager.url("https://www.saucedemo.com/v1/");
+//		System.out.println("browser launched");
+//		
+//	}
+//
+//	@AfterSuite
+//	public void teardown() {//
+//	DriverManager.close();
+//		System.out.println("browser close");
+//	}
+//	
+//		@Test(priority=3)
+//		public void productaddtocart() throws InterruptedException {
+//			Addtocart cart = new Addtocart();
+//			//cart.itemcartButton();
+//			//cart.addtocartButton();
+//			cart.container();
+//			cart.checkingItemisAvailable(productnames);
+//			List<String> item = new ArrayList<String>();
+//			item.add("Sauce Labs Backpack");
+//			item.add("Sauce Labs Bike Light");
+//			
+//			cart.checkout();
+//			
+//		}
+//		@Test(priority=4)
+//		public void processingpayment() {
+//			Payment amount = new Payment();
+//			amount.paymentofproducts("Balaji", "Balakrishnan", 605004);
+//			
+//		}
+//		
+//		@Test(priority = 5)
+//		public void overallcalculation() {
+//			FinalStatus overall=new FinalStatus();
+//			overall.calculation();
+//			overall.finish();
+//		}
 		
 
 }
